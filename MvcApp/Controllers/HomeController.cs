ac03f1e58f4700cdeb3f634a5a12ec1c6a055d24
pettijohn <pettijohn>
2013-06-12 11:22:11 +1000

    Ability to view claims

--- MvcApp/Controllers/HomeController.cs
+++ MvcApp/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Serialization;
 
 namespace MvcApp.Controllers
 {
@@ -13,6 +17,9 @@ namespace MvcApp.Controllers
 
         public ActionResult Index()
         {
+            var claimedUser = (ClaimsPrincipal)User;
+            ViewData["claims"] = claimedUser.Claims.ToArray();
+
             return View();
         }
 
