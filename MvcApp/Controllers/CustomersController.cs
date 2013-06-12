9c08cab80d9f6198b7f539946910a59e8abbf8d6
pettijohn <pettijohn>
2013-06-12 16:38:28 +1000

    Reading credentials in controller

--- MvcApp/Controllers/CustomersController.cs
+++ MvcApp/Controllers/CustomersController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MvcApp.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Web.Http;
 using System.Web.Http.OData;
 
@@ -13,9 +15,12 @@ namespace MvcApp.Controllers
         public CustomersController()
         {
             _context = new PublicODataDirect.NorthwindEntities(new Uri("http://services.odata.org/Northwind/Northwind.svc/"));
+            _knownUser = KnownUser.FromClaims(((ClaimsPrincipal)User).Claims);
         }
 
         PublicODataDirect.NorthwindEntities _context;
+        KnownUser _knownUser;
+       
 
         protected override PublicODataDirect.Customer GetEntityByKey(string key)
         {
